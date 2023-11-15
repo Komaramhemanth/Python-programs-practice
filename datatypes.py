44aed5c40415d5cbde62689e6a0af4ad7d5b7312
Komaramhemanth <140368896+Komaramhemanth@users.noreply.github.com>
2023-11-15 11:44:19 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,39 @@
+# Integer
+integer_number = 10
+print("Integer:", integer_number)
+
+# Float
+float_number = 3.14
+print("Float:", float_number)
+
+# Complex
+complex_number = 2 + 3j
+print("Complex:", complex_number)
+
+# Basic arithmetic operations
+a = 5
+b = 2
+
+# Addition
+sum_result = a + b
+print("Sum:", sum_result)
+
+# Subtraction
+difference_result = a - b
+print("Difference:", difference_result)
+
+# Multiplication
+product_result = a * b
+print("Product:", product_result)
+
+# Division
+division_result = a / b
+print("Division:", division_result)
+
+# Modulus
+modulus_result = a % b
+print("Modulus:", modulus_result)
+
+# Exponentiation
+exponentiation_result = a ** b
+print("Exponentiation:", exponentiation_result)
