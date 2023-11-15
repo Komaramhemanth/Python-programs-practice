63d6a496aaef5df39dabe8086797ddacb2a929ed
Komaramhemanth <140368896+Komaramhemanth@users.noreply.github.com>
2023-11-15 11:51:25 +0530

    Add files via upload

---
+++ string.py
@@ -0,0 +1,16 @@
+# Create a string
+string1 = "Hello, "
+string2 = "World!"
+
+# Concatenate strings
+concatenated_string = string1 + string2
+
+# Print the concatenated string
+print("Concatenated String:", concatenated_string)
+
+# Access and print a substring
+start_index = 7
+end_index = 12
+substring = concatenated_string[start_index:end_index]
+
+print("Substring:", substring)
